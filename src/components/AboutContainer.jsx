15f4245d57fa72be02643302efae09bd0ca6ed51
Roberto Luna <robertocgw@gmail.com>
2022-11-08 13:45:03 -0300

    concluido faltando as minhas informações

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,10 @@
+const AboutContainer = () => {
+  return <section id="about-container">
+    <h2>Sobre</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure hic placeat rerum dolore, neque odit perspiciatis libero reiciendis id, dolorem mollitia. Facere corporis error corrupti. Nulla iste consectetur nostrum.
+    </p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit nisi dignissimos aliquam doloremque aut debitis non libero corrupti distinctio nesciunt deserunt suscipit magnam voluptatem, illo nam voluptas hic! Fugiat, aut.</p>
+  </section>
+}
+
+export default AboutContainer
---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+const ProjectsContainer = () => {
+  return <section id="projects-container">
+    <h2>Projetos</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum molestiae iure saepe ratione neque tempora laboriosam voluptates consequuntur? Labore veniam iste perspiciatis praesentium explicabo dolorem libero odio magnam sequi adipisci?</p>
+    <a href="#" className="btn">
+      VerProjetos
+    </a>
+  </section>
+}
+
+export default ProjectsContainer
