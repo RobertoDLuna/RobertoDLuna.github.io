037e5d1ce197c2c009b9a0d328c2f63019dcae34
Roberto Luna <robertocgw@gmail.com>
2022-11-08 11:22:07 -0300

    adiçao das variaveis sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Siderbar from './components/Siderbar';
+import MainContent from './components/MainContent';
+
 import './styles/components/app.sass';
 
 function App() {
   return (
     <div id="portfolio">
       <h1>Roberto Luna</h1>
+      <Siderbar />
+      <MainContent />
     </div>
   );
 }
