9880e15c40f698650b38a9a65cd630b5ee68edbf
Roberto Luna <robertocgw@gmail.com>
2022-11-08 11:39:59 -0300

    Primeiros passos na side bar e sidebar.sass

--- src/components/Siderbar.jsx
+++ src/components/Siderbar.jsx
@@ -1,7 +1,17 @@
+import Avatar from '../img/eu.jpg'
+
+import '../styles/components/sidebar.sass'
 
 const Siderbar = () => {
   return (
-    <div>Siderbar</div>
+    <aside id="sidebar">
+      <img src={Avatar} alt="Roberto Luna" />
+      <p className="title">Desenvolvedor Full Stack</p>
+      <p>Redes Sociais</p>
+      <p>Informações para contato</p>
+      <a href="" className="btn">
+        Download Curriculo</a>
+    </aside>
   )
 }
 
